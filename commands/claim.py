dc14d2c7f6f31e08a1a790c2b9f4af8df03b3b1b
CrossyChainsaw <74303221+CrossyChainsaw@users.noreply.github.com>
2024-08-07 17:28:15 +0200

    Refactor, Fix #21

--- commands/claim.py
+++ commands/claim.py
@@ -1,18 +1,18 @@
 import json
 from Dadabase.modules.api import fetch_player_ranked_stats
 from Dadabase.classes.Link import Link
-from Dadabase.modules.data_management import codeblock_with_link_data, find_link_index, read_link_data, write_data, read_data, SERVERS_DATA_PATH
+from Dadabase.modules.data_management import embed_with_link_data, find_link_index, read_link_data, write_data, read_data, SERVERS_DATA_PATH
 
 
 async def claim(interaction, brawlhalla_id, region, country_of_residence, ethnicity):
     ranked_stats = await fetch_player_ranked_stats(brawlhalla_id)
-    user = Link(ranked_stats['brawlhalla_id'], ranked_stats['name'], interaction.user.id, interaction.user.name, region, country_of_residence, ethnicity)
+    link = Link(ranked_stats['brawlhalla_id'], ranked_stats['name'], interaction.user.id, interaction.user.name, region, country_of_residence, ethnicity)
     condition = __already_claimed(interaction)
     if condition == True:
         print('updating link')
-        await __update_link(interaction, user)
+        await __update_link(interaction, link)
     else:
-        await __add_link(interaction, user)
+        await __add_link(interaction, link)
     
 
 def __already_claimed(interaction):
@@ -25,27 +25,27 @@ def __already_claimed(interaction):
     return False
 
 
-async def __add_link(interaction, user):
+async def __add_link(interaction, link:Link):
     print('Entered: __add_link()')
-    __save_data(interaction, user)
-    await interaction.response.send_message(f"Claimed brawlhalla account {codeblock_with_link_data(user)}")
+    __save_data(interaction, link)
+    await interaction.response.send_message(f"Claimed brawlhalla account {embed_with_link_data(link, interaction)}")
 
 
-async def __update_link(interaction, user):
+async def __update_link(interaction, new_link:Link):
     print('Entered: __update_link()')
     server_data = read_data(SERVERS_DATA_PATH, interaction.guild.id)
     print(server_data)
     link_data = server_data['links']
     link_index = find_link_index(interaction.user.id, link_data)
     link = link_data[link_index]
-    link['brawlhalla_id'] = user.brawlhalla_id
-    link['brawlhalla_name'] = user.brawlhalla_name
-    link['region'] = user.region
-    link['country'] = user.country
-    link['ethnicity'] = user.ethnicity
+    link['brawlhalla_id'] = new_link.brawlhalla_id
+    link['brawlhalla_name'] = new_link.brawlhalla_name
+    link['region'] = new_link.region
+    link['country'] = new_link.country
+    link['ethnicity'] = new_link.ethnicity
     server_data['links'][link_index] = link
     write_data(SERVERS_DATA_PATH, server_data, interaction.guild.id)
-    await interaction.response.send_message(f"Updated claimed brawlhalla account {codeblock_with_link_data(user)}")
+    await interaction.response.send_message(f"Updated claimed brawlhalla account {embed_with_link_data(new_link, interaction)}")
 
 def __save_data(interaction, user):
     print('Entered: __save_data()')
