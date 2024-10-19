942bad570030c99812af8ba246a4979b2541a800
CrossyChainsaw <74303221+CrossyChainsaw@users.noreply.github.com>
2024-10-19 19:16:11 +0200

    Refactor

--- commands/claim_2v2_legend.py
+++ commands/claim_2v2_legend.py
@@ -14,6 +14,6 @@ async def claim_2v2_legend(interaction:discord.Interaction, brawlhalla_id:int, l
         a = BrawlhallaAccount(brawlhalla_id, interaction.user.name, legend_key=legend)
         data[DATA_KEY_FOR_2V2_LEGENDS].append(a.__dict__)
         write_data(CLANS_DATA_PATH, data, server_id)
-    await interaction.response.send_message('done')
+    await interaction.response.send_message(f'You claimed {legend}. This legend will appear next to your name next update.')
 
     
