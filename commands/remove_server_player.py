1dfff8977b7c03466396441beb66c09803beee4c
CrossyChainsaw <74303221+CrossyChainsaw@users.noreply.github.com>
2025-02-26 12:23:55 +0100

    refactor

--- commands/remove_server_player.py
+++ commands/remove_server_player.py
@@ -5,14 +5,14 @@ async def remove_server_player(interaction, brawlhalla_id):
     server_id = interaction.guild.id
     brawlhalla_name = ""
     server_data = read_data(SERVERS_DATA_PATH, server_id)
-    for index, item in enumerate(server_data['links']):
-        if item['brawlhalla_id'] == brawlhalla_id:
-            brawlhalla_name = server_data['links'][index]['brawlhalla_name']
-            del server_data['links'][index]
+    for i, link in enumerate(server_data['links']):
+        if link['brawlhalla_id'] == brawlhalla_id:
+            brawlhalla_name = server_data['links'][i]['brawlhalla_name']
+            del server_data['links'][i]
             write_data(SERVERS_DATA_PATH, server_data, server_id)
             await interaction.response.send_message(f"Removed brawlhalla account: {brawlhalla_name} (ID: {brawlhalla_id})")
             return
-    await interaction.respond.send_message(f"Couldn't find brawlhalla account with ID: {brawlhalla_id}")
+    await interaction.response.send_message(f"Couldn't find brawlhalla account with ID: {brawlhalla_id}")
     
 
 
