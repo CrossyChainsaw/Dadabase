747f6c9db0a260dcc1aff9b5da7c4ced105432a8
CrossyChainsaw <kaangogcay@gmail.com>
2026-04-15 23:46:29 +0200

    Help command sorts all commands alphabetically now

--- commands/help.py
+++ commands/help.py
@@ -1,9 +1,11 @@
 from Dadabase.modules.command import ALL_COMMANDS
 from discord import Interaction, Embed
 
-async def help(interaction:Interaction):
+async def help(interaction: Interaction):
     embed = Embed(title='All the Commands', description='')
-    for command in ALL_COMMANDS:
-        embed.description+=f"**/{command.name}**\n"
-        embed.description+=f"{command.description}\n"
+
+    for command in sorted(ALL_COMMANDS, key=lambda c: c.name.lower()):
+        embed.description += f"**/{command.name}**\n"
+        embed.description += f"{command.description}\n"
+
     await interaction.response.send_message(embed=embed)
